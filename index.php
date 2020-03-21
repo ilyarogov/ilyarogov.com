c95b74db5bbb2561a8e795623cff49e86ebe904f
ilyarogov <ilyarogov12@gmail.com>
2020-03-21 10:10:56 -0400

    Updated self summary. Fixed CSS on mobile.

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    
     <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Sansita:ital,wght@1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 
     <title>Ilya Rogov - Web Developer</title>
@@ -23,6 +21,7 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item il-link" href="#">PHP</a>
               <a class="dropdown-item il-link" href="#">MySQL</a>
+              <a class="dropdown-item il-link js-click" href="#">Javascript</a>
               <a class="dropdown-item il-link" href="#">Apache</a>
               <a class="dropdown-item il-link" href="#">Redis</a>
               <a class="dropdown-item il-link" href="#">.NET</a>
@@ -34,7 +33,13 @@
             <a class="nav-link il-link" href="http://www.linkedin.com/in/ilyarogov" target="_blank">LinkedIn</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link il-link" href="#">Code</a>
+            <a class="nav-link il-link" href="https://github.com/ilyarogov" target="_blank">GitHub</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link il-link" href="https://bitbucket.org/ilyarogov/" target="_blank">Bitbucket</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link il-link" href="https://ilya.wpengine.com/" target="_blank">Blog</a>
           </li>
         </ul>
       </div>
@@ -52,11 +57,38 @@
           <div class="il-title">Web Developer</div>
         </div>
       </div>
+      <br/>
+      <div class="row">
+        <div class="col align-self-center">
+          <div class="il-blurb">
+            <p>Hello, I am a professional web developer specializing <br/>
+             in the <b>LAMP</b> stack along with having some <b>.NET</b> and <b>Java</b> experience.</p>
+             <p>Visit the links at the top of the site to find out more about what I do.</p>
+             <p>You can reach me throug my LinkedIn profile.</p>
+          </div>
+        </div>
+      </div>
+      <br/>
+      <div class="row">
+        <div class="col align-self-center">
+          <div class="il-text">Copyright <?=date('Y')?></div>
+        </div>
+      </div>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script>$(()=> $('.js-click').click(()=>alert('See, I know Javascript!'); ))</script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-62744437-1', 'auto');
+      ga('send', 'pageview');
+      </script>
   </body>
 </html>
